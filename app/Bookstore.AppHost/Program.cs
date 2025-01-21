5ab62ab6754868e0403ba0ab15d04328daa62f42
NoReply Amazon <no-reply@amazon.com>
2025-01-21 09:40:53 +0000

    [Commit] Iteration 14 #errors = 2 <= 2.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/app/Bookstore.AppHost/Program.cs][CS0246][-1,-1] The type or namespace name 'Aspire' could not be found (are you missing a using directive or an assembly reference?)``.

--- app/Bookstore.AppHost/Program.cs
+++ app/Bookstore.AppHost/Program.cs
@@ -1,6 +1,6 @@
 using Aspire.Hosting;
 
-var builder = DistributedApplication.CreateBuilder(args);
+var builder = Aspire.Hosting.DistributedApplication.CreateBuilder(args);
 
 var booksDb = builder.AddSqlServer("BookstoreDbDefaultConnection");
 
